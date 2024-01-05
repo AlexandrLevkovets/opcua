6d967a4ebe69047e94266b47d2e7e00dd9b091c0
RedanKing <alexandr.levkovets@gmail.com>
2024-01-05 17:26:12 +0300

    Revert "Test node id in data channel"
    This reverts commit 1da14d9d0fe17215c58e2fa0ddd9300f6866f2a9.

--- subscription.go
+++ subscription.go
@@ -30,7 +30,6 @@ type Subscription struct {
 	RevisedLifetimeCount      uint32
 	RevisedMaxKeepAliveCount  uint32
 	Notifs                    chan<- *PublishNotificationData
-	NotifsForDataChange       chan<- *PublishNotificationDataForDataChange
 	params                    *SubscriptionParameters
 	items                     map[uint32]*monitoredItem
 	itemsMu                   sync.Mutex
@@ -79,13 +78,6 @@ func NewMonitoredItemCreateRequestWithDefaults(nodeID *ua.NodeID, attributeID ua
 			SamplingInterval: 0.0,
 		},
 	}
-}
-
-type PublishNotificationDataForDataChange struct {
-	SubscriptionID uint32
-	Error          error
-	NodeId         ua.NodeID
-	Value          interface{}
 }
 
 type PublishNotificationData struct {
@@ -277,14 +269,6 @@ func (s *Subscription) notify(ctx context.Context, data *PublishNotificationData
 	case <-ctx.Done():
 		return
 	case s.Notifs <- data:
-	}
-}
-
-func (s *Subscription) notifyForDataChange(ctx context.Context, data *PublishNotificationDataForDataChange) {
-	select {
-	case <-ctx.Done():
-		return
-	case s.NotifsForDataChange <- data:
 	}
 }
 
